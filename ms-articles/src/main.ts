99ad91d393d51fd8d04bf669ba388269851b9b52
Andres Mosquera <moscoquera@gmai.com>
2022-10-17 03:53:15 +0000

    initial microservice setup

--- ms-articles/src/main.ts
+++ ms-articles/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,
+    {
+      transport: Transport.TCP,
+      options:{
+        host:'::',
+        port:8881,
+        retryAttempts:3,
+        retryDelay: 1000
+      }
+    });
+  await app.listen();
 }
 bootstrap();
